b26ad121cefc3cd29a7f330b3d0021bbe77fa9f1
Connor Hall <connor582h@gmail.com>
2025-12-14 17:08:46 -0500

    test

--- api/email.js
+++ api/email.js
@@ -1,8 +1,6 @@
 const functions = require('../index');
 
 export function GET(request) {
-    return new Response('Hello from Vercel!');
-    /*
     functions.getRows()
     .then((response) => {
         allRows = response;
@@ -26,5 +24,4 @@ export function GET(request) {
             }
         }
     });
-    */
 }
--- index.js
+++ index.js
@@ -111,6 +111,8 @@ async function sendEmail(email, locId, days) {
     })()
 };
 
+module.exports = {getRows, sendEmail, updateDate};
+
 // get alerts
 app.get('/alert', async (req, res) => {
     // validate email- used by createAlert() and searchAlerts()
