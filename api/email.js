d778e05b2864e93b4bbe79a0b108f7352bb65abf
Connor Hall <connor582h@gmail.com>
2025-12-14 19:33:56 -0500

    test

--- api/email.js
+++ api/email.js
@@ -1,10 +1,8 @@
 const functions = require('../index');
 
 export default {
-  async fetch(request) {
-    const response = await fetch('https://api.vercel.app/products');
-    const products = await response.json();
-    return Response.json(products);
+  fetch(request) {
+    return new Response('Hello from Vercel!');
   },
 };
 /*
